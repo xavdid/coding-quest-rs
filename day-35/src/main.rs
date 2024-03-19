cdf2341f4d49d4169ac768db3a83ac07224cdbd8
David Brownman <beamneocube@gmail.com>
2024-03-18 22:18:41 -0700

    used cached crate

--- day-35/src/main.rs
+++ day-35/src/main.rs
@@ -1,7 +1,10 @@
-use num_bigint::BigInt;
-use std::collections::HashMap;
+use std::str::FromStr;
 
-fn ways_to_make(cache: &mut HashMap<BigInt, BigInt>, this_num: BigInt) -> BigInt {
+use cached::proc_macro::cached;
+use num_bigint::BigInt;
+
+#[cached]
+fn ways_to_make(this_num: BigInt) -> BigInt {
     let zero = BigInt::from(0);
     if this_num < zero {
         // overshot, bad path
@@ -13,27 +16,13 @@ fn ways_to_make(cache: &mut HashMap<BigInt, BigInt>, this_num: BigInt) -> BigInt
     }
 
     [40, 12, 2, 1]
-        .map(|offset| {
-            let next: BigInt = &this_num - offset;
-
-            match cache.get(&next) {
-                Some(result) => result.clone(),
-                None => {
-                    let result = ways_to_make(cache, next.clone());
-                    cache.insert(next.clone(), result.clone());
-                    // println!("returning {result}");
-                    result
-                }
-            }
-        })
+        .map(|offset| ways_to_make(&this_num - offset))
         .iter()
         .sum()
 }
 
-// answer is 361595632332583638761407421958897298379960745882500550853575978681928496636233758054533916390012124244431806190608039087381666468880612638124124662565287224989590899000769252066051
-// need like... huge int?
 fn main() {
-    let mut cache: HashMap<BigInt, BigInt> = HashMap::new();
-    let result = ways_to_make(&mut cache, BigInt::from(856));
+    let result = ways_to_make(BigInt::from(856));
+    assert_eq!(result, BigInt::from_str("361595632332583638761407421958897298379960745882500550853575978681928496636233758054533916390012124244431806190608039087381666468880612638124124662565287224989590899000769252066051").unwrap());
     println!("{result}");
 }
